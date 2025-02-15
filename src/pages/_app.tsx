76480d75b8c3dbbe195615edbd45455d21e5e448
Neha <pneha2296@gmail.com>
2025-02-15 14:40:24 +0530

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/admin/create-post');
+  }, []);
+
   return <Component {...pageProps} />;
 }
